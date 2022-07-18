aed17a6b2b0ee79041f65ec92ae1518052a191b3
Adrian Khaskin <haskin.dev@gmail.com>
2022-07-18 12:35:11 -0400

    Finished implementation

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,15 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.SortedSet;
 import java.util.TreeSet;
+
+import javax.management.ValueExp;
+
+/**
+ * Problems:
+ * x Write the method getNums that takes in an array of integers and return an
+ * ArrayList containing the numbers that are greater than 25.
+ */
 
 public class Main {
     public static void main(String[] args) {
@@ -9,7 +19,9 @@ public class Main {
     }
 
     public static ArrayList<Integer> getNums(int[] nums) {
-        // your code here
-        return null;
+        TreeSet<Integer> treeSet = new TreeSet<>();
+        Arrays.stream(nums).forEach(treeSet::add);
+        SortedSet<Integer> valuesAbove25 = treeSet.tailSet(Integer.valueOf(25));
+        return new ArrayList<>(valuesAbove25);
     }
 }
